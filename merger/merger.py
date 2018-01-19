0dd54a003a4dfdc7fe63dc2a117ce5faab632da5
manuelmartinezc <manuel.martinez.cebreiros@gmail.com>
2018-01-19 16:13:33 +0100

    Added stations array as a whitelist of the stations to be considered

--- merger/merger.py
+++ merger/merger.py
@@ -7,12 +7,14 @@ import errno
 
 t_names = ['s', 'y', 'doy', 'hst']
 sta_names = ['dh3', 'dh4', 'dh5', 'dh10', 'dh11', 'dh9', 'dh2', 'dh1', 'dh1tilt', 'ap6', 'ap6tilt', 'ap1', 'ap3', 'ap5', 'ap4', 'ap7', 'dh6', 'dh7', 'dh8']
+
+stations = ['dh3', 'dh4', 'dh5', 'dh10', 'dh11', 'dh9', 'dh2', 'dh1', 'ap6', 'ap1', 'ap3', 'ap5', 'ap4', 'ap7', 'dh6', 'dh7', 'dh8'] # Stations to consider
 out_folder = 'output'
 
 in_files = glob.glob('*.txt')
 
 # Create output directory if it doesn't exist
-for sta in sta_names:
+for sta in stations:
     folder = out_folder + '/' + sta
     if not os.path.exists(folder):
         os.makedirs(folder)
@@ -21,7 +23,7 @@ for in_file in in_files:
     # We use dtype=str to avoid losing precision due to data type conversions
     df = pandas.read_csv(in_file, header=None, names=t_names+sta_names, dtype=str)
 
-    for sta in sta_names:
+    for sta in stations:
         sta_df = df[ t_names + [sta] ]
 
         # WARNING: This will append the dataframe to the CSV file even if it already exists before running this script
