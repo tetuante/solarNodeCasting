959c9f17a9307bc36bb06d61f384913293502be4
manuelmartinezc <manuel.martinez.cebreiros@gmail.com>
2018-01-24 15:10:02 +0100

    Added execution progress messages and changed the path of input files

--- merger/merger.py
+++ merger/merger.py
@@ -3,19 +3,21 @@
 import pandas as pd
 import os
 import glob
-import errno
 
 t_names = ['s', 'y', 'doy', 'hst']
 sta_names = ['dh3', 'dh4', 'dh5', 'dh10', 'dh11', 'dh9', 'dh2', 'dh1', 'dh1tilt', 'ap6', 'ap6tilt', 'ap1', 'ap3', 'ap5', 'ap4', 'ap7', 'dh6', 'dh7', 'dh8']
 
 stations = ['dh3', 'dh4', 'dh5', 'dh10', 'dh11', 'dh9', 'dh2', 'dh1', 'ap6', 'ap1', 'ap3', 'ap5', 'ap4', 'ap7', 'dh6', 'dh7', 'dh8'] # Stations to consider
+in_folder = 'input'
 out_folder = 'output'
+nstations = len(stations)
 
 # Only consider data between 08:00 and 15:59 (HST)
 initial_hour = 800
 final_hour = 1559
 
-in_files = glob.glob('*.txt')
+in_files = glob.glob(in_folder + '/' + '*.txt')
+nfiles = len(in_files)
 
 stations_df = {} # Dictionary containing a dataframe per station
 
@@ -27,6 +29,7 @@ for sta in stations:
         os.makedirs(folder)
 
 for in_file in in_files:
+    print('[{}/{}] Reading {}...'.format(in_files.index(in_file)+1, nfiles, in_file))
     # We use dtype=str to avoid losing precision due to data type conversions
     df = pd.read_csv(in_file, header=None, names=t_names+sta_names, dtype={sta: str for sta in sta_names})
 
@@ -37,6 +40,8 @@ for in_file in in_files:
 
 #WARNING: this will overwrite any existing CSV file with the same path and name
 for sta in stations_df:
-    with open(out_folder + '/' + sta + '/' + sta + '.csv', 'w') as f:
+    csv = out_folder + '/' + sta + '/' + sta + '.csv'
+    with open(csv, 'w') as f:
+        print('[{}/{}] Writing {}...'.format(stations.index(sta)+1, nstations, csv))
         stations_df[sta].columns = t_names + [sta]
         stations_df[sta].to_csv(f,header=True,index=False)
