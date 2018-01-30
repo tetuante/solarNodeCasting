da2f3de31e5e501c21a895b61422191dea81ae71
manuelmartinezc <manuel.martinez.cebreiros@gmail.com>
2018-01-30 13:39:14 +0100

    Fix errors

--- merger/run_parallel.sh
+++ merger/run_parallel.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-stations = (dh3, dh4, dh5, dh10, dh11, dh9, dh2, dh1, ap6, ap1, ap3, ap5, ap4, ap7, dh6, dh7, dh8)
+stations=( dh3 dh4 dh5 dh10 dh11 dh9 dh2 dh1 ap6 ap1 ap3 ap5 ap4 ap7 dh6 dh7 dh8 )
 
-for station in "$(stations[@])";
+for station in "${stations[@]}"
 do
+    echo "Starting process for station $station"
     python3 merger.py $station &
 done
